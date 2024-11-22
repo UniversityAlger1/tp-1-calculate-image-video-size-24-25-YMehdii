11200fbe334a9c7595efbc765d463435247c4113
YMehdii <151367282+YMehdii@users.noreply.github.com>
2024-11-22 15:54:43 +0100

    Update grayScaleImage.c

--- grayScaleImage.c
+++ grayScaleImage.c
@@ -5,8 +5,6 @@
 // Return value
 //   grayScale size of the image Bitmap (in byte)
 float grayScaleImage(int w, int h) {
-   // YOUR CODE HERE - BEGIN
-
-   // YOUR CODE HERE - END
-   return 0;
+// Taille = largeur * hauteur
+    return (float)(w * h);
 }
